c5c951a150ebb7d7770e9c5cfbd48b15d1754045
akashdeb <akashdeb87@gmail.com>
2022-04-11 22:12:40 +0530

    Commit 1

---
+++ com.crm.comcast.gitPractice_SDET28/src/main/java/com/crm/comcast/gitPractice_SDET28/com/crm/comcast/gitPractice_SDET28/App.java
@@ -0,0 +1,13 @@
+package com.crm.comcast.gitPractice_SDET28.com.crm.comcast.gitPractice_SDET28;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ com.crm.comcast.gitPractice_SDET28/src/test/java/com/crm/comcast/gitPractice_SDET28/com/crm/comcast/gitPractice_SDET28/AppTest.java
@@ -0,0 +1,10 @@
+package com.crm.comcast.gitPractice_SDET28.com.crm.comcast.gitPractice_SDET28;
+
+
+
+public class AppTest 
+{
+   public static void main(String[] args) {
+	System.out.println("First Commit");
+}
+}
