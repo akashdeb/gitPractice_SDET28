c0c08711a62f54e6273f2edc370a33a456de4741
Akash <Akash@TYC1121225>
2022-04-12 00:06:33 +0530

    commit 3

---
+++ com.crm.comcast.gitPractice_SDET28/src/test/java/com/crm/comcast/gitPractice_SDET28/com/crm/comcast/gitPractice_SDET28/ClassA.java
@@ -0,0 +1,12 @@
+package com.crm.comcast.gitPractice_SDET28.com.crm.comcast.gitPractice_SDET28;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		System.out.println("Second Commit");
+		System.out.println("third Commit");
+
+
+	}
+
+}
