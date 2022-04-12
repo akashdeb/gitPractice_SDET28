9a6e40d98eff5bb4bdce6e1abdf57051faf1204b
Akash <Akash@TYC1121225>
2022-04-12 08:11:04 +0530

    Commit 4

--- com.crm.comcast.gitPractice_SDET28/src/test/java/com/crm/comcast/gitPractice_SDET28/com/crm/comcast/gitPractice_SDET28/ClassB.java
+++ com.crm.comcast.gitPractice_SDET28/src/test/java/com/crm/comcast/gitPractice_SDET28/com/crm/comcast/gitPractice_SDET28/ClassB.java
@@ -1,5 +1,7 @@
 package com.crm.comcast.gitPractice_SDET28.com.crm.comcast.gitPractice_SDET28;
 
 public class ClassB {
-
+  public static void main(String[] args) {
+	System.out.println("Commit 4");
+}
 }
